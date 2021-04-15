c54af4a602a22f141054cebb753aee3a1c28cc27
mhmxs <kovacsricsi@gmail.com>
2021-04-15 16:29:47 +0200

    Rename leader election config map

--- main.go
+++ main.go
@@ -190,7 +190,7 @@ func main() {
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
 		LeaderElection:     enableLeaderElection,
-		LeaderElectionID:   "d73494fd.storageos.com",
+		LeaderElectionID:   "api-manager-lease",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
